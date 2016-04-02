b596215aba641e9a7cd4838902da1deca0ec9703
Schneyza <schneyza.git@web.de>
2016-04-02 02:13:06 +0200

    Initiate Project
    stuff for the stuffy stuffs

---
+++ DankDudlers/Assets/Scripts/AnimateWeapon.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateWeapon : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Attack"))
+        {
+            GetComponent<Animation>().Play("AttackAnimation");
+        }
+	}
+}
