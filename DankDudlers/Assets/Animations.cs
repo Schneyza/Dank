4f6c9d197e4097745d02eed43df538d563cd6b0c
Schneyza <schneyza.git@web.de>
2016-04-03 03:50:23 +0200

    Animation testing
    added some stuff

---
+++ DankDudlers/Assets/Animations.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+    Animator anim;
+    float v;
+    float h;
+    float sprint;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        v = Input.GetAxis("Vertical");
+        sprinting();
+        transform.Translate(Vector3.forward * (Time.deltaTime * v));
+        h = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * Time.deltaTime * 200 *h);
+        /*if (Input.GetKey("a"))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime *-100);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * (100));
+        }*/
+    }
+
+    void FixedUpdate()
+    {
+        anim.SetFloat("Walk", v);
+        //anim.SetFloat("Turn", h);
+        anim.SetFloat("Sprint", sprint);
+    }
+
+    void sprinting()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            sprint = 0.2f;
+            v *= 4;
+        }
+        else
+        {
+            sprint = 0.0f;
+        }
+    }
+}
