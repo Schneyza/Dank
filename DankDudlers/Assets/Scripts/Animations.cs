2b27962691b5e687bc6a1f4971c8fcce1f5c3c46
Schneyza <schneyza.git@web.de>
2016-04-09 20:41:17 +0200

    character now wokring with collider

--- DankDudlers/Assets/Scripts/Animations.cs
+++ DankDudlers/Assets/Scripts/Animations.cs
@@ -1,24 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class Animations : MonoBehaviour {
     Animator anim;
     float v;
     float h;
     float sprint;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         v = Input.GetAxis("Vertical");
-        sprinting();
-        //transform.Translate(Vector3.forward * (Time.deltaTime * v));
         h = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * Time.deltaTime * 200 *h);
+        sprinting();
+        float angle = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
+        if (Mathf.Abs(v) > 0.01 || Mathf.Abs(h) > 0.01)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z), 20f);
+            //transform.rotation = Quaternion.Euler(Vector3.Lerp(new Vector3(0, transform.rotation.y, 0), new Vector3(0, angle, 0), 0.7f));
+            //transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
+        }
+        
+
+        //transform.Translate(Vector3.forward * (Time.deltaTime * v));
+
+        //transform.Rotate(Vector3.up * Time.deltaTime * 200 *h);
         /*if (Input.GetKey("a"))
         {
             transform.Rotate(Vector3.up * Time.deltaTime *-100);
@@ -32,7 +44,7 @@ public class Animations : MonoBehaviour {
     void FixedUpdate()
     {
         anim.SetFloat("Walk", v);
-        //anim.SetFloat("Turn", h);
+        anim.SetFloat("Turn", h);
         anim.SetFloat("Sprint", sprint);
     }
 
@@ -41,7 +53,6 @@ public class Animations : MonoBehaviour {
         if (Input.GetButton("Fire1"))
         {
             sprint = 0.2f;
-            v *= 4;
         }
         else
         {
