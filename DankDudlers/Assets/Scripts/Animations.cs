9e8f0b1ae146ff72fcff300d365613393f18e79b
Schneyza <schneyza.git@web.de>
2016-04-10 13:54:59 +0200

    basic keyboard & xbox360 controls

--- DankDudlers/Assets/Scripts/Animations.cs
+++ DankDudlers/Assets/Scripts/Animations.cs
@@ -5,40 +5,26 @@ using System.Collections;
 
 public class Animations : MonoBehaviour {
     Animator anim;
+    GameObject m_Camera;
     float v;
     float h;
     float sprint;
+    public float lookDir;
 
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
+        m_Camera = GameObject.FindGameObjectWithTag("MainCamera");
+        lookDir = 0;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        v = Input.GetAxis("Vertical");
-        h = Input.GetAxis("Horizontal");
-        sprinting();
-        float angle = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
-        if (Mathf.Abs(v) > 0.01 || Mathf.Abs(h) > 0.01)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z), 20f);
-            //transform.rotation = Quaternion.Euler(Vector3.Lerp(new Vector3(0, transform.rotation.y, 0), new Vector3(0, angle, 0), 0.7f));
-            //transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
-        }
+	
+	
+
+    void Update()
+    {
+        HandleMovAndRot();
         
-
-        //transform.Translate(Vector3.forward * (Time.deltaTime * v));
-
-        //transform.Rotate(Vector3.up * Time.deltaTime * 200 *h);
-        /*if (Input.GetKey("a"))
-        {
-            transform.Rotate(Vector3.up * Time.deltaTime *-100);
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.Rotate(Vector3.up * Time.deltaTime * (100));
-        }*/
     }
 
     void FixedUpdate()
@@ -50,13 +36,34 @@ public class Animations : MonoBehaviour {
 
     void sprinting()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Sprint") || Input.GetButton("360_rb"))
         {
             sprint = 0.2f;
         }
         else
         {
             sprint = 0.0f;
+        }
+    }
+
+    void HandleMovAndRot()
+    {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        sprinting();
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            lookDir = m_Camera.transform.eulerAngles.y;
+        }
+        else if(Input.GetJoystickNames() != null)
+        {
+            lookDir = m_Camera.transform.eulerAngles.y;
+        }
+        float angle = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg + lookDir;
+        if (Mathf.Abs(v) > 0.01 || Mathf.Abs(h) > 0.01)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z), 20f);
+
         }
     }
 }
---
+++ DankDudlers/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+    public float smooth = 1.5f;
+    public float rotateSpeed = 50f;
+    public Transform player;
+    public float test;
+    private Vector3 relCamPos;
+    private Vector3 newPos;
+    
+
+    void Awake()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        relCamPos = transform.position - player.position;
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleMovement()
+    {
+        newPos = player.position + relCamPos;
+        //transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
+        transform.position = newPos;
+    }
+
+    void HandleRotation()
+    {
+        float right_x;
+        float right_y;
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            transform.RotateAround(player.position, Vector3.up, Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed);
+            transform.RotateAround(player.position, transform.right, Input.GetAxis("Mouse Y") * Time.deltaTime * -rotateSpeed);
+            relCamPos = transform.position - player.position;
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            transform.RotateAround(player.position, Vector3.up, Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed);
+            player.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed);
+            transform.RotateAround(player.position, transform.right, Input.GetAxis("Mouse Y") * Time.deltaTime * -rotateSpeed);
+            relCamPos = transform.position - player.position;
+        }
+
+        if (Input.GetJoystickNames() != null)
+        {
+            right_x = Input.GetAxis("360_right_x");
+            right_y = Input.GetAxis("360_right_y");
+            if (Mathf.Abs(right_x) > 0.2)
+            {
+                transform.RotateAround(player.position, Vector3.up, right_x * Time.deltaTime * -rotateSpeed);
+            }
+            if (Mathf.Abs(right_y) > 0.2)
+            {
+                transform.RotateAround(player.position, transform.right, right_y * Time.deltaTime * -rotateSpeed);
+            }
+
+            relCamPos = transform.position - player.position;
+        }
+    }
+}
